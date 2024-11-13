e6320e96cf5bb75719f7119feb4b6b72e1597b3a
Carlos <carlosvermore@gmail.com>
2024-11-13 16:59:45 -0500

    Actualization

--- src/routes/personas.routes.js
+++ src/routes/personas.routes.js
@@ -27,4 +27,37 @@ router.get("/list", async (req, res) => {
   }
 });
 
+router.get("/edit/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const [persona] = await pool.query("SELECT * FROM personas WHERE id = ?", [id,]);
+    const personaEdit = persona[0];
+    res.render("personas/edit", { persona: persona[0] });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.post("/edit/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, lastname, age } = req.body;
+    const editPersona = { name, lastname, age };
+    await pool.query("UPDATE personas SET ? WHERE id = ?", [editPersona, id]);
+    res.redirect("/list");
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.get("/delete/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await pool.query("DELETE FROM personas WHERE id = ?", [id]);
+    res.redirect("/list");
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+}); 
+
 export default router;
